76c71b74e5e23c121ffd6e1ba1a82d834f877414
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-04 19:07:20 -0700

    add #initialize test

--- jungle_beat/spec/jungle_beat_spec.rb
+++ jungle_beat/spec/jungle_beat_spec.rb
@@ -2,3 +2,11 @@ require 'rspec'
 require '.lib/jung_beat'
 require '.lib/linked_list'
 require '.lib/node'
+
+RSpec.describe JungleBeat do
+  describe '#initialize' do
+    jb = JungleBeat.new
+
+    expect(jb).to be_a(JungleBeat)
+  end
+end
