c251a1e6819124645ea0d69950a0042d14ff3138
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-05 18:03:21 -0700

    Make changes to #append

--- jungle_beat/lib/jungle_beat.rb
+++ jungle_beat/lib/jungle_beat.rb
@@ -7,10 +7,13 @@ class JungleBeat
 
   def append(sounds)
     valid_sounds = 
+    stored_sounds = ''
     sounds.split(' ').each do |sound| #Splits our string and sends them on their merry way to list.append
       list.append(sound)
+      stored_sounds = stored_sounds + " #{sound}"
+      
     end
-    sounds
+    stored_sounds.lstrip! #Or just list sounds and delete line 10 and 13?
   end
 
   def count
